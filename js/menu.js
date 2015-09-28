8555576c847a899a906460074fa828c6e9719906
francoyo1998 <fprofeti98@gmail.com>
2015-09-28 01:52:51 -0300

    cambio total en el funcionamiento del menu

--- js/menu.js
+++ js/menu.js
@@ -1,42 +1,35 @@
-function sub(obj, items) {
-    console.log(123);
-    console.log(obj);
-    obj.innerHTML = "&#9660; " + obj.innerHTML.split(" ")[1];
-    console.log(obj.innerHTML.split(" ")[1]);
-    lis = [];
-    list = document.createElement("ul");
-    list.setAttribute("sm", "");
-    if(obj.getAttribute("sub") == 0){
-        console.log(obj);
-        for (i=0; i<items.length; i++) {
-            element = document.createElement("li");
-            element.setAttribute("style", "background-color: cornflowerblue;");
-            element.innerHTML = items[i];
-            list.appendChild(element);
-        }
-        console.log(this);
-        obj.parentElement.appendChild(list);
-        obj.setAttribute("sub", "1");
-        
-    }
-    else{
-        console.log("asasdasdasd");
-        obj.parentElement.removeChild(obj.parentElement.children[1]);
-        obj.innerHTML = "&#9658; " + obj.innerHTML.split(" ")[1];
-        obj.setAttribute("sub", "0");
+$(document).ready(function() {
+    
+    hideThing(".sub");
+    $(".sub").each(function () {$(this).parent().attr("class", "spl");});
+    
+    function hideThing(id) {
+        $(id).each(function () {$(this).hide()});
     }
     
-}
+    function showThing(id) {
+        $(id).each(function () {$(this).show()});
+    }
 
-$(document).ready(function(){
-    $("li").click(function(){
-       //$("li a").each(function(index){
-            if ($(this).children("a").attr("subm")){
-                console.log(this);
-                sub($(this).children("a").get(0), $(this).children("a").attr("subm").split(","));
-            }
-        //});
+    $("a").click(function () {
+        
+        if ($(this).attr("sub") == "0") {
+            cs = $(this).attr("class").split(" ");
+            showThing(".s"+cs[0][2]);
+            
+            $(this).attr("sub", "1");
+            val = $(this).html().split(" ", 2);
+            console.log(val[1]);
+            $(this).html("&#9660; " + val[1]);
+        }
+        else if ($(this).attr("sub") == "1") {
+            cs = $(this).attr("class").split(" ");
+            hideThing(".s"+cs[0][2]);
+            
+            $(this).attr("sub", "0");
+        }
     });
     
     
+
 });
