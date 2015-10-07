35f1c4da22438e533cee08f3a98b0fb15d1841ea
Rafael Cordano <rafael.cordano@gmail.com>
2015-10-06 21:40:33 -0300

    Added details link

--- js/content.js
+++ js/content.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
     window.loadContent = function(what){
         $("#content").load("templates/" + what);
     }
-
+    
+    window.showDetails = function(procStr){
+        loadContent(procStr + ".html");
+    }
 });
--- js/procesadores.js
+++ js/procesadores.js
@@ -1,39 +1,44 @@
 
 $(document).ready(function() {
-    console.log("preajax");
+    //console.log("preajax");
     $("#myTable").tablesorter({sortList: [[0,0], [1,0]]});
-    console.log("proc");
+    //console.log("proc");
     
     //headerSortDown
     $("#noth").click(function(event){
         event.preventDefault();
-        console.log("nnoth");
+        //console.log("nnoth");
     });
-    console.log("preajax");
+    //console.log("preajax");
     var imgFiles = [];
-    console.log("preajax");
+    //console.log("preajax");
     $.ajax("../img")
         .done(function(data){
-            console.log("ajax no error");
+            //console.log("ajax no error");
             $(data).find("a").each(function(){
                 var image = $(this).attr("href");
                     imgFiles.push(image);
             });
-            console.log("ajax no error");
+            //console.log("ajax no error");
             
-
+            $("table tbody tr").each(function(ind, obj){
+                var proc = $(this).attr("procimages");
+                var bk = $(this).children().first();
+                bk.html($("<a></a>").text(bk.html()).attr("href", "javascript:showDetails('" +proc+ "')"))
+            
+            });
             $("table tr").each(function(ind, obj){
                 if($(obj).attr("procimages") != undefined){
-                    console.log("if table tr");
+                    //console.log("if table tr");
                     var imgFiltered = [];
                     for(e=0;e<imgFiles.length;e++){
-                        console.log(e);
+                        //console.log(e);
                         if(imgFiles[e].match($(this).attr("procimages"))){
                             imgFiltered.push(imgFiles[e]);
                         };
                     }
-                    console.log("L31");
-                    console.log(imgFiltered);
+                    //console.log("L31");
+                    //console.log(imgFiltered);
                     strjs = "javascript:window.displayImage(0)";
 
                     $(this).prepend("<td><a href='" + strjs + "'>" + "<img src='../img/" + imgFiltered[0] + "' width='100' heigth='100'></a></td>");
@@ -44,7 +49,7 @@ $(document).ready(function() {
                 var imageArray = [];
                 var filter = $(this).parent().parent().attr("procimages");
                 for(e=0;e<imgFiles.length;e++){
-                    console.log(e);
+                    //console.log(e);
                     if(imgFiles[e].match(filter)){
                         imageArray.push(imgFiles[e]);
                     };
@@ -60,15 +65,15 @@ $(document).ready(function() {
     
     $("table th").click(function(){
         if ($(this).attr("id") != "noth"){
-            console.log($(this).html());
+            //console.log($(this).html());
             //$(".headerSortDown").html($(this).html().replace($(this).html().substr(-1), "▼"));
             //$(".headerSortUp").html($(this).html().replace($(this).html().substr(-1), "▲"));
             if($(this).hasClass("headerSortDown")){
-                console.log("d1");
+                //console.log("d1");
                 $(this).html($(this).html().replace($(this).html().substr(-1), "▼"));
             }
             else{
-                console.log("d2");
+                //console.log("d2");
                 $(this).html($(this).html().replace($(this).html().substr(-1), "▲"));
             }
         }
