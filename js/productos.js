652729b502f9155a83abb99f4af7e9f1655ce4bf
Rafael Cordano <rafael.cordano@gmail.com>
2015-10-12 21:44:17 -0300

    Fixed productos.js readability problems

--- js/productos.js
+++ js/productos.js
@@ -7,121 +7,96 @@ $(document).ready(function() {
 
     $("#noth").click(function(event){
         event.preventDefault();
-        //console.log("nnoth");
     });
-    /*var listarDirectorio = function(directorio, callback){
-        var files = [];
-        $.ajax(directorio)
-            .done(function(data){
-                $(data).find("a").each(function(){files.push($(this).attr("href"))});
-                console.log(files);
-                imgs.concat(files);
-                callback;
-            });
-    }*/
+
+    IMG_FOLDER = "../img";
+    ajaxPath = ["perifericos/controles", 
+                "perifericos/mouses",
+                "perifericos/parlantes",
+                "perifericos/teclados",
+                "procesadores/intel"
+                ];
     
-    
-    /*listarDirectorio("../img", 
-        listarDirectorio("../img/perifericos/controles",
-            listarDirectorio("../img/perifericos/mouses",
-                listarDirectorio("../img/perifericos/parlantes",
-                    listarDirectorio("../img/perifericos/teclados",
-                        function(){console.log(imgs)})))));*/
-    
-    
-        
-        $.ajax("../img")
-            .done(function(data){
+    $.ajax(IMG_FOLDER)
+        .done(function(data){
+            console.log("1stajax");
             var files = [];
-                $(data).find("a").each(function(){files.push("../"+$(this).attr("href"))});
-                console.log(files);
-                imgs.concat(files);
+            var ajaxArray = [];
+            $(data).find("a").each(function(){files.push("../"+$(this).attr("href"))});
+            console.log(files);
+            imgs.concat(files);
                 
-            $.ajax("../img/perifericos/controles")
-                .done(function(data){
-                    $(data).find("a").each(function(){files.push("../"+"perifericos/controles/" + $(this).attr("href"))});
-                    console.log(files);
-                    imgs.concat(files);
-                    $.ajax("../img/perifericos/mouses")
-                        .done(function(data){
-                            $(data).find("a").each(function(){files.push("../" + $(this).attr("href"))});
-                            console.log(files);
-                            imgs.concat(files);
+            for(e=0;e<ajaxPath.length;e++){
+                ajaxArray.push(
+                    $.ajax({
+                        url: IMG_FOLDER+"/"+ajaxPath[e],
+                        success: function(data){
+                            $(data).find("a").each(
+                                function(){
+                                    files.push(".." + $(this).attr("href"));
+                                }
+                            );
                             
-                            $.ajax("../img/perifericos/parlantes")
-                                .done(function(data){
-                                    $(data).find("a").each(function(){files.push("../"+$(this).attr("href"))});
-                                    console.log(files);
-                                    imgs.concat(files);
-                                    $.ajax("../img/perifericos/teclados")
-                                        .done(function(data){
-                                            $(data).find("a").each(function(){files.push("../"+$(this).attr("href"))});
-                                            console.log(files);
-                                            imgs.concat(files);
-                                            console.log("ttt");
-    $("table tbody tr").each(function(ind, obj){
-        var proc = $(this).attr("procimages");
-        var bk = $(this).children().first();
-        bk.html($("<a></a>").text(bk.html()).attr("href", "javascript:showDetails('" +proc+ "')"))
-            
-    });
-            $("table tr").each(function(ind, obj){
-                if($(obj).attr("procimages") != undefined){
-                    imgs = files;
-                    window.images = imgs;
-                    //console.log("if table tr");
-                    var imgFiltered = [];
+                            imgs.concat(files);
+                            console.log(files);
+                        }
+                        
+                    })
+                );
+                    
+            }
+            window.images = files;
+            $.when.apply($,ajaxArray).done(function(){
+                console.log(files);
+                $("table tbody tr").each(function(ind, obj){
+                    var proc = $(this).attr("procimages");
+                    var bk = $(this).children().first();
+                    bk.html($("<a></a>").text(bk.html()).attr("href", "javascript:showDetails('" +proc+ "')"))
+
+                });
+                $("table tr").each(function(ind, obj){
+                    if($(obj).attr("procimages") != undefined){
+                        imgs = files;
+                        var imgFiltered = [];
+                        for(e=0;e<imgs.length;e++){
+                            if(imgs[e].match($(this).attr("procimages"))){
+                                imgFiltered.push(imgs[e]);
+                            };
+                        }
+
+                        strjs = "javascript:window.displayImage(0)";
+                        console.log(imgFiltered[0]);
+                        $(this).prepend("<td><a href='" + strjs + "'>" + "<img src='../img/" + imgFiltered[0] + "' width='100' heigth='100'></a></td>");
+                    }
+                });
+
+                $("td a").click(function(){
+                    var imageArray = [];
+                    var filter = $(this).parent().parent().attr("procimages");
                     for(e=0;e<imgs.length;e++){
-                        //console.log(e);
-                        if(imgs[e].match($(this).attr("procimages"))){
-                            imgFiltered.push(imgs[e]);
+                        if(imgs[e].match(filter)){
+                            imageArray.push(imgs[e]);
                         };
                     }
-                    
-                    strjs = "javascript:window.displayImage(0)";
-                    console.log(imgFiltered[0]);
-                    $(this).prepend("<td><a href='" + strjs + "'>" + "<img src='../img/" + imgFiltered[0] + "' width='100' heigth='100'></a></td>");
-                }
-            });
-            
-            $("td a").click(function(){
-                var imageArray = [];
-                var filter = $(this).parent().parent().attr("procimages");
-                for(e=0;e<imgs.length;e++){
-                    //console.log(e);
-                    if(imgs[e].match(filter)){
-                        imageArray.push(imgs[e]);
-                    };
-                }
-                /*console.log("<<<<<<<<<<<<<");
-                console.log(window.image.getAttribute('width'));
-                console.log(window.image.getAttribute('heigth'));
-                console.log(">>>>>>>>>>>>>");*/
-                setRoot('../img/');
-                setImageArray(imageArray);
-                setImage(0);
-            });
+                    setRoot(IMG_FOLDER);
+                    setImageArray(imageArray);
+                    setImage(0);
+                });
 
-    $("table th").click(function(){
-        if ($(this).attr("id") != "noth"){
+                $("table th").click(function(){
+                    if ($(this).attr("id") != "noth"){
 
-            if($(this).hasClass("headerSortDown")){
-                //console.log("d1");
-                $(this).html($(this).html().replace($(this).html().substr(-1), "▼"));
-            }
-            else{
-                //console.log("d2");
-                $(this).html($(this).html().replace($(this).html().substr(-1), "▲"));
-            }
-        }
-        
-    });
-                                        });
-                                });
-                            
-                            
-                        });
+                        if($(this).hasClass("headerSortDown")){
+                            $(this).html($(this).html().replace($(this).html().substr(-1), "▼"));
+                        }
+                        else{
+                            $(this).html($(this).html().replace($(this).html().substr(-1), "▲"));
+                        }
+                    }
+
                 });
             });
+        });
+    
 
 });
