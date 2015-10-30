fa94fa6eb4cf0255033acaabf9c2bd5b698a5397
Rafael Cordano <rafael.cordano@gmail.com>
2015-10-30 08:44:20 -0300

    Fixed compatibility problems

--- js/show.js
+++ js/show.js
@@ -11,7 +11,8 @@ $(document).ready(function(){
     window.displayImage = function(i) {
         window.image.setAttribute('src', '../img/'+window.imageArray[i]);
         $("body").css("overflow", "hidden");
-        $("body").scrollTop(0);
+        $("body").scrollTop(0); //works on chrome
+	$(window).scrollTop(0); //works on firefox
         $("body").append(window.fullBack);
         $("body").append(window.fullImage)
         $("body").append(window.next);
